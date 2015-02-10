84bdba62233fc3c1782c81796d49ffe307469c86
Fotios Lindiakos <flindiakos@shutterstock.com>
2015-02-10 00:33:07 -0500

    Turns out I didn't need to persist configs, just data

--- docker/cassandra/scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Restore base configs if they don't exist
-# TODO: Check to see if we have configs already
-# TODO: Process args in case we want to clear the configs
-cp -r /config/cassandra/* /etc/cassandra
-
-cassandra-singlenode
